afb6fdffab0463711ebf4bf458ff6e3f771b372e
Cesar <cesarvargasescorcia@gmail.com>
2020-07-30 14:19:52 -0500

    node-mongo:configurando el motor de platillas y mostrando el estado de las respuestas del servidor

--- src/app.js
+++ src/app.js
@@ -7,9 +7,13 @@ const colors = require('colors');
 //Settings - configuraciones
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views')); //declarando la ruta de las vistas
+app.set('view engine', 'ejs'); //declarando el uso de las plantillas ejs
 
-//Middlewares
-
+//Middlewares - funciones que se ejecutan antes de que lleguen a las rutas
+/**
+ * morgan: muestra el estado de respuesta de el servidor a las peticiones
+ * ejemplo: GET / 404 21.972 ms - 139 */
+app.use(morgan('dev'));
 
 //Routes
 
