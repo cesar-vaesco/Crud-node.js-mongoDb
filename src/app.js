c46332d3e5ed4f671253024f63b35789957bc90f
Cesar <cesarvargasescorcia@gmail.com>
2020-07-30 13:58:26 -0500

    node-mongo: creando servidor - dando estructura de carpetas a nuestro proyecto - instalando módulos

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+//Settings - configuraciones
+
+
+//Middlewares
+
+
+//Routes
+
+
+//Inicializando el servidor
+
+app.listen(3000, () => {
+    console.log(`Servidor creado en el puerto 3000`);
+});
